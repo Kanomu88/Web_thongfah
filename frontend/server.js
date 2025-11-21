745c9e97ae685e5bf3d461f7954cea8efcdb01d0
Jirawat2105 <128300044+Jirawat2105@users.noreply.github.com>
2025-11-21 11:35:10 +0700

    navbar updated

---
+++ frontend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+// #################################################
+// Auth routes
+// #################################################
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'search.html'));
+});
+
+app.get('/product', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'product.html'));
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend server running on http://localhost:${PORT}`);
+});
